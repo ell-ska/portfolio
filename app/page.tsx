8b89dc9f8e84ed236cb43c98e78aab61d3811c7a
ell-ska <ella@skagerstrom.se>
2023-08-11 14:42:45 +0200

    animated header

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,19 @@ const Home = async () => {
   const projects: HomepageProject[] = await getHomepageProjects()
 
   return (
-    <main className='flex flex-col p-6 md:p-20'>
+    <>
       <Header></Header>
-      <h1 className='mt-16 text-4xl md:mt-32 md:text-6xl md:leading-tight'>
-        {short_description}
-      </h1>
-      <section className='mt-16 flex flex-wrap gap-12 md:mt-24 md:gap-2'>
-        {projects.map(project => (
-          <ProjectCard key={project._id} {...project} />
-        ))}
-      </section>
-    </main>
+      <main className='flex flex-col px-6 pb-6 pt-14 md:px-20 md:pb-20 md:pt-28'>
+        <h1 className='mt-16 text-4xl md:mt-32 md:text-6xl md:leading-tight'>
+          {short_description}
+        </h1>
+        <section className='mt-16 flex flex-wrap gap-12 md:mt-24 md:gap-2'>
+          {projects.map(project => (
+            <ProjectCard key={project._id} {...project} />
+          ))}
+        </section>
+      </main>
+    </>
   )
 }
 
--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import { PortableText } from '@portabletext/react'
 import { ArrowUpRight, MoveRight } from 'lucide-react'
 import { getNextProject, getProject } from '@/sanity/routes'
+import formatHexColor from '@/utils/formatHexColor'
+import Header from '@/components/Header'
 import Banner from '@/components/Banner'
 import type Project from '@/sanity/types/project'
 
@@ -12,48 +14,51 @@ const Work = async ({ params: { slug } }: { params: { slug: string } }) => {
   )
 
   return (
-    <main>
-      <Banner
-        images={project.phone_images}
-        accentColor={project.accent_color}
-      />
-      <section className='mt-12 grid grid-cols-12 gap-8 px-6 md:mt-16 md:px-20'>
-        <h2 className='col-span-full text-4xl font-bold md:col-span-3'>
-          {project.name}
-        </h2>
-        <div className='prose prose-lg col-span-full max-w-none md:col-span-9'>
-          <PortableText value={project.description} />
-          <div className='not-prose mt-8 flex gap-12 font-bold'>
-            {project.links?.live_project && (
-              <Link
-                href={project.links?.live_project}
-                className='flex items-center gap-2'>
-                <ArrowUpRight className='inline' />
-                <span>Live project</span>
-              </Link>
-            )}
-            {project.links?.source_code && (
-              <Link
-                href={project.links?.source_code}
-                className='flex items-center gap-2'
-                style={{ color: project.accent_color }}>
-                <ArrowUpRight className='inline' />
-                <span>Source code</span>
-              </Link>
-            )}
+    <>
+      <Header initiallyHidden={true} />
+      <main>
+        <Banner
+          images={project.phone_images}
+          accentColor={project.accent_color}
+        />
+        <section className='mt-12 flex flex-col justify-between gap-8 px-6 md:mt-16 md:flex-row md:px-20'>
+          <h2 className='whitespace-nowrap text-4xl font-bold'>
+            {project.name}
+          </h2>
+          <div className='prose prose-lg max-w-4xl'>
+            <PortableText value={project.description} />
+            <div className='not-prose mt-8 flex gap-12 font-bold'>
+              {project.links?.live_project && (
+                <Link
+                  href={project.links?.live_project}
+                  className='flex items-center gap-2'>
+                  <ArrowUpRight className='inline' />
+                  <span>Live project</span>
+                </Link>
+              )}
+              {project.links?.source_code && (
+                <Link
+                  href={project.links?.source_code}
+                  className='flex items-center gap-2'
+                  style={{ color: formatHexColor(project.accent_color) }}>
+                  <ArrowUpRight className='inline' />
+                  <span>Source code</span>
+                </Link>
+              )}
+            </div>
           </div>
-        </div>
-      </section>
-      <section className='mb-8 mt-12 px-6 md:px-20'>
-        <span>Next project</span>
-        <Link
-          href={`/work/${nextProject.slug.current}`}
-          className='flex items-center justify-between gap-4 md:justify-normal'>
-          <h3 className='text-3xl font-bold'>{nextProject.name}</h3>
-          <MoveRight />
-        </Link>
-      </section>
-    </main>
+        </section>
+        <section className='mb-8 mt-12 px-6 md:px-20'>
+          <span>Next project</span>
+          <Link
+            href={`/work/${nextProject.slug.current}`}
+            className='flex items-center justify-between gap-4 md:justify-normal'>
+            <h3 className='text-3xl font-bold'>{nextProject.name}</h3>
+            <MoveRight />
+          </Link>
+        </section>
+      </main>
+    </>
   )
 }
 
