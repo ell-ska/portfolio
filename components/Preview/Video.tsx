a81202f57341c8d0195340c05d4672046bf3f7d2
ell-ska <ella@skagerstrom.se>
2023-09-11 14:57:03 +0200

    remove border on small screens

--- components/Preview/Video.tsx
+++ components/Preview/Video.tsx
@@ -1,6 +1,6 @@
 const Video = ({ url }: { url: string }) => {
   return (
-    <div className='overflow-hidden rounded-b-md border border-t-0 border-neutral-300 md:rounded-b-xl'>
+    <div className='overflow-hidden rounded-b-md border-t-0 border-neutral-300 md:rounded-b-xl md:border'>
       <video src={url} playsInline autoPlay muted loop></video>
     </div>
   )
