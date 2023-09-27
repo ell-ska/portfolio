9ef5f1d982cf4998bf42f346959e3c709b1f9751
ell-ska <ella@skagerstrom.se>
2023-09-27 18:14:43 +0200

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Outfit } from 'next/font/google'
+import ogImage from '@/public/og.jpg'
 import './globals.css'
 import type { Metadata } from 'next'
 
@@ -6,9 +7,25 @@ const outfit = Outfit({ subsets: ['latin'], variable: '--font-outfit' })
 
 export const metadata: Metadata = {
   title: 'ellska',
-  description: 'Frontend Developer',
+  description:
+    'Ellska is a frontend developer with a passion for building modern and beautiful websites',
   icons: {
     icon: '/favicon.svg',
+  },
+  openGraph: {
+    title: 'ellska',
+    description:
+      'Ellska is a frontend developer with a passion for building modern and beautiful websites',
+    url: 'https://ellska.dev',
+    siteName: 'ellska.dev',
+    images: [
+      {
+        url: ogImage.src,
+        width: 2800,
+        height: 1600,
+      },
+    ],
+    type: 'website',
   },
 }
 
