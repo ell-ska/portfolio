87ee83cf1ffcda3e905b2cdd2737f321061e1f9c
ell-ska <ella@skagerstrom.se>
2025-07-27 23:13:55 +0200

    fix: remove comment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 export default {
   images: {
     remotePatterns: [
-      // new URL('https://cdn.sanity.io/images/9bvrpa1x/production/**'),
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
